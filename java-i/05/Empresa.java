60fef389d866c4ecd8c54c21f356b60aa39162f6
Caue Queiroz <cauenqueiroz@gmail.com>
2017-12-19 14:59:51 -0200

    Finish module 05

---
+++ java-i/05/Empresa.java
@@ -0,0 +1,41 @@
+class Empresa {
+    String nome;
+    String cnpj;
+    Funcionario[] funcionarios = new Funcionario[10];
+    int vagaLivre = 0;
+
+    void adicionar(Funcionario funcionario) {
+        this.funcionarios[this.vagaLivre] = funcionario;
+        this.vagaLivre++;
+    }
+
+    void mostraFuncionarios() {
+        for(int i=0; i<this.vagaLivre; i++) {
+            Funcionario funcionario = this.funcionarios[i];
+
+            System.out.println("-----");
+            System.out.println(funcionario.getNome());            
+        }
+    }
+
+    public Funcionario getFuncionario(int posicao) {
+        return this.funcionarios[posicao];
+    }
+
+    void mostraTodasAsInformacoes() {
+        for(int i=0; i<this.vagaLivre; i++) {
+            Funcionario funcionario = this.funcionarios[i];
+            funcionario.getInfo();
+        }
+    }
+
+    boolean contem(Funcionario funcionario) {
+        for(int i=0; i<this.vagaLivre; i++) {
+            if (this.funcionarios[i] == funcionario) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
---
+++ java-i/05/FuncionarioTeste.java
@@ -0,0 +1,9 @@
+class FuncionarioTeste {
+    public static void main(String[] args) {
+        Funcionario caue = new Funcionario();
+        caue.setNome("Caue Queiroz");
+        caue.setSalario(800);
+
+        caue.getInfo();        
+    }
+}
