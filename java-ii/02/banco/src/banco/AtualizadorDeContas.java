e1dcf4918dd91b806ba27fd75a417f29661d358a
Caue Queiroz <cauenqueiroz@gmail.com>
2017-12-20 20:03:35 -0200

    Rewrite another exercise from module 02

---
+++ java-ii/02/banco/src/banco/AtualizadorDeContas.java
@@ -0,0 +1,23 @@
+package banco;
+
+public class AtualizadorDeContas {
+	private double saldoTotal = 0;
+	private double selic;
+	
+	public AtualizadorDeContas(double selic) {
+		this.selic = selic;
+	}
+	
+	public double getSaldoTotal() {
+		return this.saldoTotal;
+	}
+	
+	public void roda(Conta c) {
+		System.out.println("===========================================");
+		System.out.println("Saldo anterior: R$"+c.getSaldo());
+		c.atualiza(this.selic);
+		System.out.println("Saldo atual: R$"+c.getSaldo());
+		
+		this.saldoTotal += c.getSaldo();
+	}
+}
---
+++ java-ii/02/banco/src/banco/ContaCorrente.java
@@ -0,0 +1,7 @@
+package banco;
+
+public class ContaCorrente extends Conta {
+	public void atualiza(double taxa) {
+		this.saldo += this.saldo * taxa * 2;
+	}
+}
