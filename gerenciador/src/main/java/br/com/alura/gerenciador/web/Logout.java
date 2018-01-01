fcd5ea281b318e261bab67dfc1307a90bbba2351
Caue Queiroz <cauenqueiroz@gmail.com>
2018-01-01 05:34:41 -0200

    Finish module 09

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -1,11 +1,9 @@
 package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,13 +15,7 @@ public class Logout extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		req.getSession().removeAttribute("usuario.logado");
-
-		PrintWriter writer = resp.getWriter();
-		writer.println("<html><body>");
-		writer.println("<h1>Logout</h1>");
-		writer.println("<p>Usuario deslogado com sucesso!</p>");
-		writer.println("<a href='/gerenciador/'>Voltar</a>");
-		writer.println("</body></html>");
+		req.getRequestDispatcher("/WEB-INF/pages/logout.html").forward(req, resp);
 
 	}
 
