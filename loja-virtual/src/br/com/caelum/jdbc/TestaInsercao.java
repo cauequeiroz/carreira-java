4d13d13a07518c650f669350d31dee14574cda62
Caue Queiroz <cauenqueiroz@gmail.com>
2018-01-06 02:40:27 -0200

    Finish module 04

--- loja-virtual/src/br/com/caelum/jdbc/TestaInsercao.java
+++ loja-virtual/src/br/com/caelum/jdbc/TestaInsercao.java
@@ -8,26 +8,38 @@ import java.sql.Statement;
 
 public class TestaInsercao {
 	public static void main(String[] args) throws SQLException {
-		
-		String nome = "Nintendo Switch 5.5');";
-		String descricao = "Console portatil da Nintendo.";
-		String sql = "insert into Produto (nome, descricao) values (?, ?)";
-		
-		Connection connection = Database.getConnection();
-		PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-		
+
+		try (Connection connection = Database.getConnection()) {
+			connection.setAutoCommit(false);
+			String sql = "insert into Produto (nome, descricao) values (?, ?)";
+
+			try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+				adiciona("Nintendo Switch", "Teste de SQL Injection'); delete from Produto", stmt);
+				adiciona("Caue", "Queiroz", stmt);
+				connection.commit();
+			} catch(Exception error) {
+				error.printStackTrace();
+				connection.rollback();
+				System.out.println("Rolback efetuado");
+			}
+		}
+
+	}
+
+	private static void adiciona(String nome, String descricao, PreparedStatement stmt) throws SQLException {
+
+		if (nome.equals("Caue")) {
+			throw new IllegalArgumentException("Erro proposital.");
+		}
+
 		stmt.setString(1, nome);
 		stmt.setString(2, descricao);
 		stmt.execute();
-		
-		ResultSet generatedKeys = stmt.getGeneratedKeys();
-		while(generatedKeys.next()) {
-			System.out.println(generatedKeys.getInt("id") + " gerado!");
+
+		try(ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+			while (generatedKeys.next()) {
+				System.out.println(generatedKeys.getInt("id") + " gerado!");
+			}			
 		}
-		
-		generatedKeys.close();
-		stmt.close();
-		connection.close();
-		
 	}
 }
